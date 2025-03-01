9ba7ca42abaa24e0a06f91a890f894f53530851d
SAFALKSHARMA <np03cs4s230215@heraldcollege.edu.np>
2025-03-01 15:05:27 +0545

    added controller to get the all users data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,33 +1,51 @@
 import userModel from "../models/userModel.js";
 
 export const getUserData = async (req, res) => {
-    try {
-        const { userId } = req.body;
+  try {
+    const { userId } = req.body;
 
-        // Ensure userId is provided
-        if (!userId) {
-            return res.json({ success: false, message: 'User ID is required' });
-        }
-
-        // Find user by the provided userId
-        const user = await userModel.findById(userId);
-
-        if (!user) {
-            return res.json({ success: false, message: 'User not found' });
-        }
-
-        // Return the user data
-        res.json({
-            success: true,
-            userData: {
-                name: user.name,
-                email: user.email,
-                isAccountVerified: user.isAccountVerified
-            }
-        });
-
-    } catch (error) {
-        // Return the error message in the response
-        res.json({ success: false, message: error.message });
+    // Ensure userId is provided
+    if (!userId) {
+      return res.json({ success: false, message: "User ID is required" });
     }
-}
+
+    // Find user by the provided userId
+    const user = await userModel.findById(userId);
+
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    // Return the user data
+    res.json({
+      success: true,
+      userData: {
+        name: user.name,
+        email: user.email,
+        isAccountVerified: user.isAccountVerified,
+      },
+    });
+  } catch (error) {
+    // Return the error message in the response
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getAllUsers = async (req, res) => {
+  try {
+    // Fetch only verified users
+    const users = await userModel.find(
+      { isAccountVerified: true },
+      "name email role isAccountVerified"
+    );
+
+    // Return the filtered users' data
+    res.json({
+      success: true,
+      users,
+    });
+  } catch (error) {
+    // Handle errors
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import userAuth from '../middlewares/userAuth.js';
-import { getUserData } from '../controllers/userController.js';
+import express from "express";
+import userAuth from "../middlewares/userAuth.js";
+import { getUserData, getAllUsers } from "../controllers/userController.js";
 
-const userRouter = express.Router()
+const userRouter = express.Router();
 
-userRouter.get('/data', userAuth, getUserData)
+userRouter.get("/data", userAuth, getUserData);
+userRouter.get("/all-users", getAllUsers);
 
 export default userRouter;
